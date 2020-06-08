4a7a75cbcd7c842970ca7e9a6689f9ca110c8542
youssefwork <youssef.samih97@gmail.com>
2020-06-08 03:53:48 +0100

    third party library like class validator and transformer and how to integrated with @types library's

---
+++ libs-03-class-transformer/src/product.model.ts
@@ -0,0 +1,18 @@
+import { IsEmpty, IsNumber, IsPositive } from "class-validator";
+
+export class Product {
+  @IsEmpty()
+  title: string;
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+
+  getInformation() {
+    return [this.title, `$${this.price}`];
+  }
+}
--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -1,6 +1,31 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
+// import { ProjectInput } from "./components/project-input";
+// import { ProjectList } from "./components/project-list";
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+// new ProjectInput();
+// new ProjectList('active');
+// new ProjectList('finished');
+
+// import _ from 'lodash';
+
+// console.log(_.shuffle([1, 2, 3]));
+import "reflect-metadata";
+import { plainToClass } from 'class-transformer';
+import { Product } from "./product.model";
+
+
+// const p1 = new Product('A Book', 12.99);
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 }
+];
+
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
